32a26a6b37a7ecf8fcd7e6f26dfefa3b67819ee1
nsainto <nsainton@student.42.fr>
2023-10-23 02:08:39 +0200

    feat(man_pages_section): :fire: Removed useless code and added a tiny docstring :fire:

--- man_pages_sections/man_pages_section.sh
+++ man_pages_sections/man_pages_section.sh
@@ -4,21 +4,15 @@
 
 export MAN_PAGES_SECTION=1
 
-colorcodes="colorcodes.sh"
-utils="utils/utils.sh"
-remote_utils="https://raw.githubusercontent.com/nsainton/Scripts/file_splitting/""$utils"
-echo "$remote_utils"
-. "../utils/utils.sh"
-. "man_splitting.sh"
-#curl -s "$remote_utils"
-#if [ ! -f "$utils" ] ; then . <(curl -s "$remote_utils") ; fi
-
+:<<-'MAN_SECTION'
+	Opens a man page in an optionnal man section to a given section
+	Example: man_section man.7 Fonts
+	MAN_SECTION
 man_section(){
 	declare	page
 	declare	section="${*:2}"
 	declare usage="Usage: man_section page section"
 
-	source_file  "$colorcodes"
 	find_page_section "$1"
 	if [ "$section" = "" ] ; then echo -e "$usage" ; return ; fi
 	page="$ret_val"
@@ -27,15 +21,3 @@ man_section(){
 	pick_section "$ret_val" "$(build_regex "$section")"
 	cut_man "$page" "$section" "$ret_val"
 }
-
-main(){
-	local ret_val
-	man_section "$@"
-}
-
-#main "$@"
-declare ret_val
-find_page_section "$1"
-print_sections "$ret_val"
-#find_page_section "$1"
-#build_regex "$*"
