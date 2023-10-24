b3c719e3d0cda3ebc0c585e91bc8c2f3a409c23d
lazydev <nsainton@student.42.fr>
2023-10-24 19:28:29 +0200

    fix(shell_lint_check): :heavy_plus_sign: Added docker image for shellcheck :heavy_plus_sign:

--- utils/shell_lint_check.mk
+++ utils/shell_lint_check.mk
@@ -1,12 +1,12 @@
 # **************************************************************************** #
 #                                                                              #
 #                                                         :::      ::::::::    #
-#    Makefile                                           :+:      :+:    :+:    #
+#    shell_lint_check.mk                                :+:      :+:    :+:    #
 #                                                     +:+ +:+         +:+      #
 #    By: nsainto <nsainton@student.42.fr>           +#+  +:+       +#+         #
 #                                                 +#+#+#+#+#+   +#+            #
 #    Created: 2023/09/19 14:18:51 by nsainto           #+#    #+#              #
-#    Updated: 2023/09/19 15:43:43 by nsainto          ###   ########.fr        #
+#    Updated: 2023/09/20 13:34:28 by nsainton         ###   ########.fr        #
 #                                                                              #
 # **************************************************************************** #
 
@@ -15,29 +15,38 @@ GRN=\e[0;32m
 BLU=\e[0;34m
 CRESET=\e[0m
 
+SHELLCHECK := $(shell which shellcheck)
+PWD := $(shell pwd)
+DOCKER_SHELLCHECK := docker run --rm -v "$(PWD):/mnt" koalaman/shellcheck:stable
+
+ifeq "$(SHELLCHECK)" ""
+$(info shellcheck not found, running using docker)
+$(info more infos at "https://github.com/koalaman/shellcheck")
+SHELLCHECK := $(DOCKER_SHELLCHECK)
+endif
+
 SHELL = /usr/bin/bash
 
 SHELL_FILES := $(shell find . -path "*.sh")
 
-define lint_check = 
-LOGS="$@.log"
-shellcheck $(1) > $$LOGS
-RESULT=$$?
-if [ $$RESULT -ne 0 ]
-then
-	printf "%b\n" "$(RED) Errors found in $(BLU) $(1) $(RED), checking stops $(CRESET)"
-	cat $$LOGS
-	rm -f $$LOGS
-	exit 1
-else
-	printf "%b\n" "$(GRN) No error found in $(BLU) $(1) $(CRESET)"
-	rm -f $$LOGS
-fi
+define lint_check
+
+LOGS="$@.log"; \
+$(SHELLCHECK) $(1) > "$$LOGS"; \
+RESULT=$$?; \
+if [ $$RESULT -ne 0 ]; \
+then \
+	printf "%b\n" "$(RED) Errors found in $(BLU) $(1) $(RED), checking stops $(CRESET)"; \
+	cat $$LOGS; \
+	rm -f $$LOGS; \
+	exit 1; \
+else \
+	printf "%b\n" "$(GRN) No error found in $(BLU) $(1) $(CRESET)"; \
+	rm -f $$LOGS; \
+fi \
 
 endef
 
 .PHONY: check
 check:
 	@$(foreach arg, $(SHELL_FILES), $(call lint_check,$(arg)))
-
-.ONESHELL:
