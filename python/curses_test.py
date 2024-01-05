e107c96300142c08c02939756cf32bd95e299598
nsainto <nsainton@student.42.fr>
2024-01-05 01:05:40 +0100

    test(curses): :alembic: Added test for newwin and pads :alembic:

--- python/curses_test.py
+++ python/curses_test.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 import curses
+import time
 
 # Init the curses screen and returns a window object 
 # representing the entire screen
@@ -34,4 +35,32 @@ def main(stdscr):
 	stdscr.refresh()
 	stdscr.getkey()
 
-curses.wrapper(main)
+try:
+	curses.wrapper(main)
+except ZeroDivisionError:
+	print("You can't divide by zero my G")
+
+begin_x = 20; begin_y = 7
+height = 5; width = 40
+win = curses.newwin(height, width, begin_y, begin_x)
+win.noutrefresh()
+curses.doupdate()
+time.sleep(1)
+win.clear()
+curses.endwin()
+
+pad = curses.newpad(100, 100)
+
+for y in range(0, 99):
+	for x in range(0, 99):
+		pad.addch(y, x, ord('a') + (x * x + y * y) % 26)
+
+# Displays a section of the pad in the middle of the screen
+# (0,0) : coordinate of upper-left corner of pad area to display
+# (5,5) : coordinate of upper-left corner of window area to be filled
+#			with pad content.
+# (20, 75) : coordinate of lower-right corner of window area to be
+#			filled with pad content.
+pad.refresh(0, 0, 5, 5, 20, 75)
+time.sleep(1)
+curses.endwin()
