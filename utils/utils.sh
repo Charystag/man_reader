322fa7d86167059e2595ed0f3b0364081c21f259
nsainto <nsainton@student.42.fr>
2024-03-18 12:55:49 +0100

    feat(colorcodes): :construction: Started modifying repository to change colorcodes :construction:

--- utils/utils.sh
+++ utils/utils.sh
@@ -3,6 +3,13 @@
 
 export UTILS=1
 
+if [ "$COLORCODES" -ne 1 ]
+then
+	if [ ! -f "colorcodes.sh" ] ; then curl --connect-timeout 10 -fsSL \
+"https://raw.githubusercontent.com/nsainton/man_reader/master/utils/colorcodes.sh" | . /dev/stdin;
+	else . "colorcodes.sh" ; fi
+fi
+
 #-------------------------------------------------------------------------------
 #------------------------------------INPUT--------------------------------------
 #-------------------------------------------------------------------------------
@@ -49,12 +56,12 @@ is_int(){
 	Forces `less` to use colors to display man pages
 	COLORED_MAN
 man() {
-LESS_TERMCAP_md=$'\e[01;31m' \
-LESS_TERMCAP_me=$'\e[0m' \
-LESS_TERMCAP_us=$'\e[01;32m' \
-LESS_TERMCAP_ue=$'\e[0m' \
+LESS_TERMCAP_md="$BRED" \
+LESS_TERMCAP_me=$'$CRESET' \
+LESS_TERMCAP_us=$'$BGRN' \
+LESS_TERMCAP_ue=$'$CRESET' \
 LESS_TERMCAP_so=$'\e[45;93m' \
-LESS_TERMCAP_se=$'\e[0m' \
+LESS_TERMCAP_se=$'$CRESET' \
 command man "$@"
 }
 
